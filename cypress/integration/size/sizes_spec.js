a20ad0edbb17b7b59b9f3da5e69461d0d3ae141e
Cesar Ramirez <carr.gdl1@gmail.com>
2022-02-04 18:41:52 -0600

    Sizes component tests

--- cypress/integration/size/sizes_spec.js
+++ cypress/integration/size/sizes_spec.js
@@ -10,11 +10,11 @@ describe(`Sizes Component`,()=> {
         //Should have 7 sizes
         cy.get('div[class="filters-available-size"]').should('have.length', 7);
 
-        const sizes = ["XS","S","M","L","XL","XXL"]
-
         //Verify Sizes text
-        cy.get('div[class="filters-available-size"]').each((el) => {
-             el.text() === sizes[el];
-        });
+        cy.get('div[class="filters-available-size"]')
+        .should("have.text", "XSSMMLLXLXXL");
+
+        //Sizes subtext
+        cy.get("small").should("contains.text","Leave a star on Github if this repository was useful :)");
     });
 });
